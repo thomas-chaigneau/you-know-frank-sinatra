a53743c997172037ebf0ef13559495eb2f4709b4
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2019-01-30 12:28:36 +0100

    hello word ok

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require_relative "database"
 
+print DB
+print '-------------------'
+print COMMENTS
+
 get "/" do
   @database = DB
+  erb :home
 end
