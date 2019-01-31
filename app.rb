b9d82a253671cb8b61e12d850d6434a37c1c6bb1
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2019-01-31 17:11:08 +0100

    store users ok

--- app.rb
+++ app.rb
@@ -2,7 +2,28 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require_relative "database"
 
+def store_usersNames(filename, usersName)
+  File.open(filename, "a+") do |file|
+    file.puts(usersName)
+  end
+end
+
+def read_usersNames(filename)
+  return File.read(filename).split("\n")
+end
+
 get "/" do
+  erb :login
+end
+
+post "/" do
+  @usersName = params["usersName"]
+  store_usersNames("users.txt", @usersName)
+  redirect "/home"
+end
+
+get "/home" do
+  @usersNames = read_usersNames("users.txt")
   @articles = DB
   @comments = COMMENTS
   erb :home
